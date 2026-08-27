f3f6a84d2e594096a8bab416fc966ea74cc7de99
Kostiantyn Babich (Connie) <babich.kostia@gmail.com>
2026-08-27 15:30:18 -0700

    IVT layout

--- src/main.c
+++ src/main.c
@@ -1,19 +1,24 @@
-#define RCC_APB2ENR (*(volatile unsigned *)0x40021018)
-#define GPIOC_CRH   (*(volatile unsigned *)0x40011004)
-#define GPIOC_ODR   (*(volatile unsigned *)0x4001100c)
+#include <stdint.h>
+// register address
+#define RCC_BASE 0x40021000
+#define GPIOC_BASE 0x40011000
+#define RCC_APB2ENR *(volatile uint32_t *)(RCC_BASE + 0x18)
+#define GPIOC_CRH *(volatile uint32_t *)(GPIOC_BASE + 0x04)
+#define GPIOC_ODR *(volatile uint32_t *)(GPIOC_BASE + 0x0C)
+// bit fields
+#define RCC_IOPCEN (1 << 4)
+#define GPIOC13 (1UL << 13)
 
-#define IOPCEN   (1u << 4)
-#define LED_PIN  13
-#define PC13_MODE_OUT_2MHZ (2u << 20)
-#define PC13_CFG_MASK      (0xFu << 20)
-
-int main(void) {
-  RCC_APB2ENR |= IOPCEN;
-  GPIOC_CRH = (GPIOC_CRH & ~PC13_CFG_MASK) | PC13_MODE_OUT_2MHZ;
-
+void main(void) {
+  RCC_APB2ENR |= RCC_IOPCEN;
+  GPIOC_CRH &= 0xFF0FFFFF;
+  GPIOC_CRH |= 0x00200000;
   while (1) {
-    GPIOC_ODR ^= 1u << LED_PIN;
-    for (volatile unsigned i = 0; i < 500000u; i++)
-      ;
+    GPIOC_ODR |= GPIOC13;
+    for (int i = 0; i < 500000; i++)
+      ; // arbitrary delay
+    GPIOC_ODR &= ~GPIOC13;
+    for (int i = 0; i < 500000; i++)
+      ; // arbitrary delay
   }
 }
